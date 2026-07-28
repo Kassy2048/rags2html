965a52b96497e5c8576d726d6e90bc4bb4319321
Kassy <78169097+Kassy2048@users.noreply.github.com>
2026-07-28 21:52:19 +0200

    Replace match with if/elif chain
    There is no significant performances boost when using match, and this is
    not supported by "old" Python versions.

--- compat.py
+++ compat.py
@@ -178,23 +178,24 @@ class Color:
 
         if text.startswith('Color ['):
             text = text[7:-1]
+
         if text.startswith('#'):
             text = text[1:]
-            match len(text):
-                case 3:
-                    # rgb, set alpha to FF and convert as aarrggbb
-                    text = "FF" + text[0] * 2 + text[1] * 2 + text[2] * 2
-                case 4:
-                    # argb, convert as aarrggbb
-                    text = text[0] * 2 + text[1] * 2 + text[2] * 2 + text[3] * 2
-                case 6:
-                    # rrggbb, set alpha to FF
-                    text = "FF" + text
-                case 8:
-                    # aarrggbb
-                    pass
-                case _:
-                    raise RuntimeError('Invalid hex color code "%s"' % (_text,))
+            size = len(text)
+            if size == 3:
+                # rgb, set alpha to FF and convert as aarrggbb
+                text = "FF" + text[0] * 2 + text[1] * 2 + text[2] * 2
+            elif size == 4:
+                # argb, convert as aarrggbb
+                text = text[0] * 2 + text[1] * 2 + text[2] * 2 + text[3] * 2
+            elif size == 6:
+                # rrggbb, set alpha to FF
+                text = "FF" + text
+            elif size == 8:
+                # aarrggbb
+                pass
+            else:
+                raise RuntimeError('Invalid hex color code "%s"' % (_text,))
 
             return signed(int(text, 16))
 
